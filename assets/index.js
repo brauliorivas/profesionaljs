d541595b59ac9603b9837031c1d862f0e4f5ca72
brauliorivas <brauliorivas2004@gmail.com>
2021-09-29 10:35:18 -0500

    intersection observer

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,13 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector('video');
 const button = document.getElementById('playpause-button');
 const mute = document.getElementById('mute-button');
 
 const player = new MediaPlayer({el: video, 
-    // plugins: [new AutoPlay]
+    plugins: [new AutoPlay(), new AutoPause()]
 },'pause');
 
 button.onclick = () => player.status == "pause" ? player.play() : player.pause();
